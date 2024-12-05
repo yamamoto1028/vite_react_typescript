cff1816807c6e57804bf951169d75ac3c731db61
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-12-05 11:42:41 +0900

    ✨ Headerコンポーネントの実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { requests } from "./request";
 import { Row } from "./components/Row/index";
 import { Banner } from "./components/Banner";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     // 追加箇所
     <div className="App">
+      <Header />
       <Banner />
       <Row
         title={"NetflixOriginals"}
