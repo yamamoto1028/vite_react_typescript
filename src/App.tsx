1b4e9f40f30162ca94a75822adba869c0a5a21ab
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2025-05-03 15:08:38 +0900

    ✨ reacrt-queryのインストール

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,31 @@ import { requests } from "./request";
 import { Row } from "./components/Row/index";
 import { Banner } from "./components/Banner";
 import { Header } from "./components/Header";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient(); // react-queryを定義
 
 function App() {
   return (
-    // 追加箇所
-    <div className="App">
-      <Header />
-      <Banner />
-      <Row
-        title={"NetflixOriginals"}
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow={true}
-      />
-      <Row title={"TopRated"} fetchUrl={requests.fetchTopRated} />
-      <Row title={"ActionMovies"} fetchUrl={requests.fetchActionMovies} />
-      <Row title={"NewsMovies"} fetchUrl={requests.fetchNewsMovies} />
-      <Row title={"KidsMovies"} fetchUrl={requests.fetchKidsMovies} />
-      <Row title={"DocumentMovies"} fetchUrl={requests.fetchDocumentMovies} />
-      <Row title={"RomanceMovies"} fetchUrl={requests.fetchRomanceMovies} />
-      <Row title={"Trending"} fetchUrl={requests.fetchTrending} />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      {/* トップレベルでラップする */}
+      <div className="App">
+        <Header />
+        <Banner />
+        <Row
+          title={"NetflixOriginals"}
+          fetchUrl={requests.fetchNetflixOriginals}
+          isLargeRow={true}
+        />
+        <Row title={"TopRated"} fetchUrl={requests.fetchTopRated} />
+        <Row title={"ActionMovies"} fetchUrl={requests.fetchActionMovies} />
+        <Row title={"NewsMovies"} fetchUrl={requests.fetchNewsMovies} />
+        <Row title={"KidsMovies"} fetchUrl={requests.fetchKidsMovies} />
+        <Row title={"DocumentMovies"} fetchUrl={requests.fetchDocumentMovies} />
+        <Row title={"RomanceMovies"} fetchUrl={requests.fetchRomanceMovies} />
+        <Row title={"Trending"} fetchUrl={requests.fetchTrending} />
+      </div>
+    </QueryClientProvider>
   );
 }
 
