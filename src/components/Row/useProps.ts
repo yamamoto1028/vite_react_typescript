3fbc3a0242e5f8ad7fb4f88df787d8d5c139e7d8
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2025-05-03 17:46:25 +0900

    🐛 エラー除去

--- src/components/Row/useProps.ts
+++ src/components/Row/useProps.ts
@@ -64,7 +64,7 @@ export const useProps = (fetchUrl: string, title: string) => {
   //   }
   // };
   return {
-    movies,
+    movies: movies ?? [],
     trailerUrl,
     handleClick,
     isLoading,
