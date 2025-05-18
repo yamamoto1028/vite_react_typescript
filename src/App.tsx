3d66bf3f009678f6769e495d519795ebe5075038
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2025-05-18 12:35:43 +0900

    ✨ useContextの導入・予告映像、映像情報、音声On/Offをグローバルステートで管理

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,34 @@ import { Row } from "./components/Row/index";
 import { Banner } from "./components/Banner";
 import { Header } from "./components/Header";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BannerDataProvider } from "./BannerDataContext";
 
 const queryClient = new QueryClient(); // react-queryを定義
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      {/* トップレベルでラップする */}
-      <div className="App">
-        <Header />
-        <Banner />
-        <Row
-          title={"NetflixOriginals"}
-          fetchUrl={requests.fetchNetflixOriginals}
-        />
-        <Row title={"TopRated"} fetchUrl={requests.fetchTopRated} />
-        <Row title={"ActionMovies"} fetchUrl={requests.fetchActionMovies} />
-        <Row title={"NewsMovies"} fetchUrl={requests.fetchNewsMovies} />
-        <Row title={"KidsMovies"} fetchUrl={requests.fetchKidsMovies} />
-        <Row title={"DocumentMovies"} fetchUrl={requests.fetchDocumentMovies} />
-        <Row title={"RomanceMovies"} fetchUrl={requests.fetchRomanceMovies} />
-        <Row title={"Trending"} fetchUrl={requests.fetchTrending} />
-      </div>
+      <BannerDataProvider>
+        {/* BannerDataProviderで全体をラップする*/}
+        <div className="App">
+          <Header />
+          <Banner />
+          <Row
+            title={"NetflixOriginals"}
+            fetchUrl={requests.fetchNetflixOriginals}
+          />
+          <Row title={"TopRated"} fetchUrl={requests.fetchTopRated} />
+          <Row title={"ActionMovies"} fetchUrl={requests.fetchActionMovies} />
+          <Row title={"NewsMovies"} fetchUrl={requests.fetchNewsMovies} />
+          <Row title={"KidsMovies"} fetchUrl={requests.fetchKidsMovies} />
+          <Row
+            title={"DocumentMovies"}
+            fetchUrl={requests.fetchDocumentMovies}
+          />
+          <Row title={"RomanceMovies"} fetchUrl={requests.fetchRomanceMovies} />
+          <Row title={"Trending"} fetchUrl={requests.fetchTrending} />
+        </div>
+      </BannerDataProvider>
     </QueryClientProvider>
   );
 }
